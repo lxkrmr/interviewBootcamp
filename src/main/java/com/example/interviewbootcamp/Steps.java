9b2a9824e349b476d7a4a990316e5cc204fab4a5
Alex <lxkrmr@icloud.com>
2022-05-05 16:07:44 +0200

    Added alternative solution for exercise #9 Steps

--- src/main/java/com/example/interviewbootcamp/Steps.java
+++ src/main/java/com/example/interviewbootcamp/Steps.java
@@ -6,19 +6,23 @@ import java.util.stream.IntStream;
 
 public class Steps {
 
-    public static List<String> steps(int numberOfSteps) {
-        return IntStream.range(1, numberOfSteps + 1)
-                        .mapToObj(currentStep -> createStep(currentStep, numberOfSteps))
+    public static List<String> steps(int steps) {
+        return IntStream.range(0, steps)
+                        .mapToObj(currentRow -> createRow(currentRow, steps))
                         .collect(Collectors.toList());
     }
 
-    private static String createStep(int currentStep, int numberOfSteps) {
-        return createString('#', currentStep) + createString(' ', numberOfSteps - currentStep);
+    private static String createRow(int currentRow, int steps) {
+        return IntStream.range(0, steps)
+                        .mapToObj(currentColumn -> createCell(currentRow, currentColumn))
+                        .collect(Collectors.joining());
     }
 
-    private static String createString(Character character, int times) {
-        return IntStream.range(0, times)
-                        .mapToObj(ignore -> String.valueOf(character))
-                        .collect(Collectors.joining());
+    private static String createCell(int currentRow, int currentColumn) {
+        if (currentColumn <= currentRow) {
+            return "#";
+        }
+
+        return " ";
     }
 }
